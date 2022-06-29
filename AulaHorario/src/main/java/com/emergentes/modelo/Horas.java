5df242742f052d9e6d6a0cbf36efd9d99e31b598
Angelazio <angelcoronelmarquez@gmail.com>
2022-06-28 23:05:02 -0400

    PROYECTO

---
+++ AulaHorario/src/main/java/com/emergentes/modelo/Horas.java
@@ -0,0 +1,31 @@
+package com.emergentes.modelo;
+public class Horas {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRango() {
+        return rango;
+    }
+
+    public void setRango(String rango) {
+        this.rango = rango;
+    }
+    private String rango;
+
+    public Horas() {
+    }
+
+    @Override
+    public String toString() {
+        return "Horas{" + "id=" + id + ", rango=" + rango + '}';
+    }
+    
+    
+}
